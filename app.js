661df29b1f8563de6543cf22d1fbdc84f41ca765
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-08-29 12:16:37 +0530

    item table create

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Customer = require('./routes/Customer')
+const Item = require('./routes/Item')
 
 const app = express();
 const port = 4000;
@@ -7,6 +8,7 @@ const port = 4000;
 app.use(express.json())
 
 app.use('/customer',Customer)
+app.use('/item',Item)
 
 app.listen(port,(req,res) => {
     console.log('App start and listening on port :'+port)
